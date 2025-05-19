237fded6438604675572f078fb2e30d5610272c7
tomdave01 <tomdave01@gmail.com>
2025-05-19 05:02:31 +0200

    initial commit

---
+++ src/extract/extract_team_stats.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import json
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv('FBREF_BASE_URL')
+API_KEY = os.getenv('FBREF_API_KEY')
+params = {
+            'league_id': '9' ,  
+            'season_id': '2024-2025'
+        }
+HEADERS = {'X-API-KEY': API_KEY}
+
+def get_team_stats():
+    """
+    Fetches team statistics for the specified league and season.
+    """
+    params = {
+            'league_id': '9' ,  
+            'season_id': '2024-2025'
+        }
+    HEADERS = {'X-API-KEY': API_KEY}
+    response = requests.get(f'{API_URL}/team-season-stats', params=params, headers=HEADERS)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+get_team_stats()
