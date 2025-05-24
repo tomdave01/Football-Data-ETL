e277336f0518c8ebd05e899ee208339f74dc3a8e
tomdave01 <tomdave01@gmail.com>
2025-05-24 16:06:51 +0200

    Refactor player stats transformation to include detailed statistics categories and improve data handling

--- src/transform/transform_player_stats.py
+++ src/transform/transform_player_stats.py
@@ -17,6 +17,16 @@ def transform_player_stats(input_file: str, output_file: str) -> None:
     if not player_files:
         print("No player statistics files found.")
         raise FileNotFoundError("No player statistics files found.")
+    
+    player_stats_df = pd.DataFrame()
+    player_shooting_df = pd.DataFrame()
+    player_passing_df = pd.DataFrame()
+    player_passing_types_df = pd.DataFrame()
+    player_goal_creation_actions_df = pd.DataFrame()
+    player_defense_df = pd.DataFrame()
+    player_possession_df = pd.DataFrame()
+    player_playing_time_df = pd.DataFrame()
+    player_miscellaneous_df = pd.DataFrame()
 
     for player_file in player_files:
         try:
@@ -26,6 +36,94 @@ def transform_player_stats(input_file: str, output_file: str) -> None:
 
             with open(input_file, 'r') as file:
                 raw_data = json.load(file)
+            
+            for player_data in raw_data['players']:
+                player_id = player_data['meta_data']['player_id']
+                player_name = player_data['meta_data']['player_name']
+                player_country_code = player_data['meta_data']['player_country_code']
+                player_age = player_data['meta_data']['age']
+        
+                stats = pd.json_normalize(player_data["stats"]["stats"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "stats"
+                player_stats_df = pd.concat([player_stats_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["shooting"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "shooting"
+                player_shooting_df = pd.concat([player_shooting_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["passing"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "passing"
+                player_passing_df = pd.concat([player_passing_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["passing_types"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "passing_types"
+                player_passing_types_df = pd.concat([player_passing_types_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["gca"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "goal_creation_actions"
+                player_goal_creation_actions_df = pd.concat([player_goal_creation_actions_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["defense"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "defense"
+                player_defense_df = pd.concat([player_defense_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["possession"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "possession"
+                player_possession_df = pd.concat([player_possession_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["playingtime"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "playing_time"
+                player_playing_time_df = pd.concat([player_playing_time_df, stats], ignore_index=True)
+
+                stats = pd.json_normalize(player_data["stats"]["misc"])
+                stats['player_id'] = player_id
+                stats['player_name'] = player_name
+                stats['team_name'] = team_name
+                stats['player_country_code'] = player_country_code
+                stats['player_age'] = player_age
+                stats['section'] = "miscellaneous"
+                player_miscellaneous_df = pd.concat([player_miscellaneous_df, stats], ignore_index=True)
+
         except FileNotFoundError:
             print(f"Error: The file '{input_file}' was not found.")
             return
@@ -33,20 +131,13 @@ def transform_player_stats(input_file: str, output_file: str) -> None:
             print(f"Error: The file '{input_file}' is not a valid JSON.")
             return
 
-    player_stats_df = pd.DataFrame()
-    
-    for player_data in raw_data['data']:
-        player_id = player_data['meta_data']['player_id']
-        player_name = player_data['meta_data']['player_name']
-        
-        # Flatten the stats and add player_id and player_name
-        stats = pd.json_normalize(player_data["stats"])
-        stats['player_id'] = player_id
-        stats['player_name'] = player_name
-        
-        # Concatenate the current player's stats with the main DataFrame
-        player_stats_df = pd.concat([player_stats_df, stats], ignore_index=True)
-
     # Save the transformed data to a CSV file
+    output_file = output_file or 'data/processed/player_stats.csv'
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
     player_stats_df.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    input_file = 'data/raw/players_stats.json'
+    output_file = 'data/processed/player_stats.csv'
+    transform_player_stats(input_file, output_file)
+    print(f"Transformed player statistics saved to {output_file}")
